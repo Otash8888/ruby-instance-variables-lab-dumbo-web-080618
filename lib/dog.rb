a8243e7b6c976f40f1d3c93a467a46c68600a926
Otabek Kamalov <otakam86@gmail.com>
2018-08-08 17:45:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
   
-  attr_accessor :dog_name
-  def initialize (dog_name)
-   @dog_name = dog_name 
-  end
+  # attr_accessor :dog_name
+  # def initialize (dog_name)
+  # @dog_name = dog_name 
+  # end
 end 
